8d9e4d156935ee65b8e5813e69a1de3aa9de08a7
Yi-Ru Lan <a78945612385238@gmail.com>
2022-06-30 22:53:59 +0800

    Update progress

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 "use strict";
+const users = [
+    { name: "Ian", age: 23 },
+    { name: "Owen", age: 24 },
+];
+const user = users.sort((a, b) => b.age - a.age)[0];
+console.log(user);
+const delay = new Promise((resolve) => setTimeout(resolve, 2000));
---
+++ app.ts
@@ -0,0 +1,8 @@
+const users = [
+  { name: "Ian", age: 23 },
+  { name: "Owen", age: 24 },
+];
+
+const user = users.sort((a, b) => b.age - a.age)[0];
+console.log(user);
+const delay = new Promise((resolve) => setTimeout(resolve, 2000));
